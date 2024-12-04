248e1ae6e88684d609fabee890f0be125198a2ff
SIDDA NAGA RAVINDRA <154605228+RAVINDRA0022@users.noreply.github.com>
2024-12-04 20:39:57 +0530

    DECIMAL TO BINARY

---
+++ decimal_to_binary.v
@@ -0,0 +1,6 @@
+`timescale 1ns / 1ps
+
+module decimal_to_binary( input [3:0] decimal_in,
+                          output[3:0] binary_out );
+                  assign binary_out = decimal_in;
+endmodule
